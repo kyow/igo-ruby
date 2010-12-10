ea8ffab7aae1f623156e6d1ee6172345816fd79c
K.Nishi <24signals@gmail.com>
2010-12-11 02:11:21 +0900

    辞書ファイルを移動し、test.rbが参照する辞書ファイルのパスを変更。

--- test/test.rb
+++ test/test.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'igo-ruby'
-tagger = Igo::Tagger.new('../ipadic')
+tagger = Igo::Tagger.new('../../ipadic')
 t = tagger.parse('吾輩は猫である。名前はまだ無い。')
 t.each{|m|
   puts "#{m.surface} #{m.feature} #{m.start}"
