50895dc8118d2f4f30f352dc996664e9729c523d
K.Nishi <24signals@gmail.com>
2011-02-12 06:05:57 +0900

    整形

--- test/test.rb
+++ test/test.rb
@@ -7,10 +7,12 @@ puts "version -> #{Igo::Version.igo_ruby}"
 
 tagger = Igo::Tagger.new(File.dirname(__FILE__) + '/../../ipadic')
 t = tagger.parse('吾輩は猫である。名前はまだ無い。')
+
 puts "parse ->"
 t.each{|m|
   puts "#{m.surface} #{m.feature} #{m.start}"
 }
+
 puts "wakati ->"
 t = tagger.wakati('どこで生れたかとんと見当がつかぬ。')
 puts t.join(' ')
